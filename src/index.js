3af7375172f069e8b517f412ca0dac9923f34443
Masegolex <160056223+Masegolex@users.noreply.github.com>
2024-06-16 12:14:26 +0200

    Added the basic form handling and added basic typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: ["Purposefully remaining calm"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
